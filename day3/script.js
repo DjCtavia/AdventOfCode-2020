d02d596e81882c270041c821d0037f3bc16c1389
DjCtavia <djctavia@gmail.com>
2020-12-19 17:09:02 +0100

    Day 3, part 2

--- day3/script.js
+++ day3/script.js
@@ -18,19 +18,34 @@ readInterface.on('line', (line) => {
 readInterface.on('close', () => {
     mapXLimit = map[0].length;
     Part1();
+    Part2();
 });
 
-function Part1() {
-    console.log("---Part 1 ---");
+function FightTheSlope(xSpeed, ySpeed) {
     let curPos = {x: 0, y: 0};
-    let posSpeed = {x: 3, y: 1};
     let hitedTrees = 0;
 
     while (curPos.y < map.length) {
         if (map[curPos.y][curPos.x] === '#')
             hitedTrees++;
-        curPos.x = (curPos.x + posSpeed.x) % mapXLimit;
-        curPos.y += posSpeed.y;
+        curPos.x = (curPos.x + xSpeed) % mapXLimit;
+        curPos.y += ySpeed;
     }
-    console.log(`Number of hited trees: ${hitedTrees}`);
+    return hitedTrees;
+}
+
+function Part1() {
+    console.log("--- Part 1 ---");
+    console.log(`Number of hited trees: ${FightTheSlope(3, 1)}`);
+}
+
+function Part2() {
+    console.log("--- Part 2 ---");
+    const slope1 = FightTheSlope(1, 1);
+    const slope2 = FightTheSlope(3, 1);
+    const slope3 = FightTheSlope(5, 1);
+    const slope4 = FightTheSlope(7, 1);
+    const slope5 = FightTheSlope(1, 2);
+
+    console.log(`Let's multiply the trees we took in our face last times... ${slope1 * slope2 * slope3 * slope4 * slope5}`);
 }
